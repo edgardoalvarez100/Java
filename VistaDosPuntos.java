2568987f086bf1407455f92053a653c869f45acb
Edgardo Alvarez <edgardoalvarez100@gmail.com>
2013-03-21 19:32:24 -0500

    Create VistaDosPuntos.java

---
+++ VistaDosPuntos.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package software2.patterns.mvc.imp;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import software2.patterns.mvc.abst.Controlador;
+import software2.patterns.mvc.abst.Modelo;
+import software2.patterns.mvc.abst.Vista;
+
+
+public class VistaDosPuntos extends Vista {
+
+    public Vista2Puntos(Modelo m) {
+        super(m);
+
+    }
+
+    @Override
+    protected void dibujar() {
+        System.out.println("Mostrar datos en consola (2 puntos)");
+        Modelo m = this.obtenerModelo();
+        Map map = m.obtenerMapa();
+        Set<String> keys = map.keySet();
+        Iterator<String> it = keys.iterator();
+        while (it.hasNext()) {
+            String s = it.next();
+            System.out.print(s);
+            System.out.print("::::::::::::::::" + map.get(s));
+            System.out.println();
+        }
+
+    }
+
+    @Override
+    protected Controlador construirControlador() {
+        return new ControladorDosPuntos(this);
+    }
+}
